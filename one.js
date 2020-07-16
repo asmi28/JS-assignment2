dda62e9ae99618a8454f8df47b7db7fd322a71ba
asmi28 <68271816+asmi28@users.noreply.github.com>
2020-07-16 11:59:28 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,12 @@
+let userObject ={
+    name:"John",
+    age:25,
+    isFunctional:true
+    
+
+}
+if (userObject.age>=21) {
+    console.log('This user can drink!');
+}else{
+    console.log('This user cannot drink!');
+}
