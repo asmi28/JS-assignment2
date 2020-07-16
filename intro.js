c2c79835f29ed97e7c6d329c4575e6d926422f72
asmi28 <68271816+asmi28@users.noreply.github.com>
2020-07-16 12:01:50 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,22 @@
+//Number
+let num=33;
+console.log(num);
+
+//string
+let str="Asmita";
+console.log(str);
+
+//Boolean
+let bool=true;
+console.log(bool);
+
+//Bigint
+let num1=12345678n;
+console.log(num1);
+
+//null
+let nullvar=null;
+console.log(nullvar);
+
+
+
